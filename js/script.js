50825d158e58ef825fb9cb2aee333094b96e79eb
uietcsw <shubhkirti.work@gmail.com>
2017-09-30 14:02:15 +0530

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-$(window).load(function(){
-  if($(window).scrollTop()>1){
-    $(".navbar-theme").css("background-color", "#1E003D");
-    $(".navbar-theme").css("border-bottom", "1px solid #222");
-    $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
-  }
-  else{
-    $(".navbar-theme").css("background-color", "transparent");
-    $(".navbar-theme").css("border-bottom", "1px solid transparent");
-    $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
-  }
-});
-
-$(window).scroll(function() {
-  if($(window).scrollTop()>100){
-    $(".navbar-theme").css("background-color", "#1E003D");
-    $(".navbar-theme").css("border-bottom", "1px solid #222");
-    $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
-  }
-  else{
-    $(".navbar-theme").css("background-color", "transparent");
-    $(".navbar-theme").css("border-bottom", "1px solid transparent");
-    $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
-  }
-});
+// $(window).load(function(){
+//   if($(window).scrollTop()>1){
+//     $(".navbar-theme").css("background-color", "#1E003D");
+//     $(".navbar-theme").css("border-bottom", "1px solid #222");
+//     $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
+//   }
+//   else{
+//     $(".navbar-theme").css("background-color", "transparent");
+//     $(".navbar-theme").css("border-bottom", "1px solid transparent");
+//     $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
+//   }
+// });
+//
+// $(window).scroll(function() {
+//   if($(window).scrollTop()>100){
+//     $(".navbar-theme").css("background-color", "#1E003D");
+//     $(".navbar-theme").css("border-bottom", "1px solid #222");
+//     $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
+//   }
+//   else{
+//     $(".navbar-theme").css("background-color", "transparent");
+//     $(".navbar-theme").css("border-bottom", "1px solid transparent");
+//     $(".navbar-dark .navbar-nav .nav-link").css("color", "#ffffff");
+//   }
+// });
 
 $(document).ready(function(){
   $('#move-home').click(function(event){
@@ -31,6 +31,6 @@ $(document).ready(function(){
   });
   $('#move-to-about').click(function(event){
     event.preventDefault();
-    $('html, body').animate({scrollTop: 600}, 350);
+    $('html, body').animate({scrollTop: 700}, 350);
   });
 });
